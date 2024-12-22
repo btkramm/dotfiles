4842dfff68724162950b43fa3871b2d2678e75c3
Benjamín Kramm <btkramm@gmail.com>
2024-12-22 00:51:54 -0300

    feat: integrate "fzf" and "bat"

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -3,7 +3,7 @@ source <(fzf --zsh)
 export FZF_DEFAULT_COMMAND='fd --type f'
 
 export FZF_CTRL_T_COMMAND='fd --type f'
-export FZF_CTRL_T_OPTS='--walker-skip .git,node_modules'
+export FZF_CTRL_T_OPTS="--walker-skip .git,node_modules --preview 'bat -n --color=always {}'"
 
 export FZF_ALT_C_COMMAND='fd --type d'
 export FZF_ALT_C_OPTS="--walker-skip .git,node_modules --preview 'tree -C {}'"

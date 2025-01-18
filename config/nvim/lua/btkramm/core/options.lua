db406acc0f67855f002a0988bab66fb7376c8676
Benjamín Kramm <btkramm@gmail.com>
2025-01-18 01:08:14 -0300

    feat: set "shortmess"

--- config/nvim/lua/btkramm/core/options.lua
+++ config/nvim/lua/btkramm/core/options.lua
@@ -5,6 +5,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.shiftwidth = 2
+vim.opt.shortmess:append('I')
 vim.opt.smartcase = true
 vim.opt.swapfile = false
 vim.opt.updatetime = 300

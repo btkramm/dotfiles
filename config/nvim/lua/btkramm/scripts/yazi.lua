d2bc252d0a63a93458cf2d14fff3cd356acfda50
Benjamín Kramm <btkramm@gmail.com>
2025-03-29 18:16:20 -0300

    fix: remove "yazi-path" on "ExitPre"

--- config/nvim/lua/btkramm/scripts/yazi.lua
+++ config/nvim/lua/btkramm/scripts/yazi.lua
@@ -8,3 +8,12 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     vim.fn.writefile({ e.file }, path)
   end,
 })
+
+vim.api.nvim_create_autocmd({ 'ExitPre' }, {
+  group = 'yazi',
+  callback = function(e)
+    local path = os.getenv('XDG_CONFIG_HOME') .. '/yazi-path'
+
+    vim.fn.delete(path)
+  end,
+})

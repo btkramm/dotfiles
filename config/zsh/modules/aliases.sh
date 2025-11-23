c46b06f6994e19a19b4f69c50a93d69dfbf9a065
Benjamín Kramm <btkramm@gmail.com>
2025-11-23 14:38:40 -0300

    feat: install "ast-grep"

--- config/zsh/modules/aliases.sh
+++ config/zsh/modules/aliases.sh
@@ -6,6 +6,8 @@ alias ls='EZA_CONFIG_DIR=$XDG_CONFIG_HOME/eza eza'
 
 alias pwb="git rev-parse --abbrev-ref HEAD"
 
+alias sg="ast-grep"
+
 alias ssh="kitten ssh"
 
 alias sync="brew update && brew upgrade && brew cleanup && ya pack -u"

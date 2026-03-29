d1fc4130c8b0983ec82599de74126737c3d856f5
Benjamín Kramm <btkramm@gmail.com>
2026-03-29 19:45:17 -0300

    fix(nvim): replace `Comment.nvim` with `folke/ts-comments.nvim`
    `nvim-ts-context-commentstring` crashes on Neovim 0.12 because
    `vim.treesitter.get_parser()` now returns nil instead of throwing.
    Switch to `ts-comments.nvim` which uses Neovim's native commenting and
    is actively maintained.

--- config/nvim/lua/btkramm/plugins/editing/comment.lua
+++ config/nvim/lua/btkramm/plugins/editing/comment.lua
@@ -1,11 +1,6 @@
 return {
-  'numToStr/Comment.nvim',
-  dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  'folke/ts-comments.nvim',
+  event = 'VeryLazy',
 
-  config = function()
-    ---@diagnostic disable: missing-fields
-    require('Comment').setup({
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    })
-  end,
+  config = true,
 }
